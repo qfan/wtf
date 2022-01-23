38d3750cd7d308ae107690b044515ca04a2a0063
Qi Fan <fanqi1234@gmail.com>
2022-01-22 16:12:00 -0800

    Create killbells.bash

---
+++ killbells.bash
@@ -0,0 +1,5 @@
+echo 'set noerrorbells visualbell t_vb=' >> /etc/vim/vimrc.local
+echo 'autocmd GUIEnter * set visualbell t_vb=' >> /etc/vim/vimrc.local
+
+echo 'set bell-style visible' >> ~/.inputrc
+echo 'set bell-style visible' >> /etc/inputrc
